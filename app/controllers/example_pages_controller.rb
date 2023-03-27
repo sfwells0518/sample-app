3079fba6535ec25fe916dfd42e0ac15f415f1349
Sam Wells <sfwells0518@gmail.com>
2023-03-27 12:37:15 -0500

    render with test html

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,5 +1,12 @@
 class ExamplePagesController < ApplicationController
   def plants
-    render json: { message: "hello!" }
+    # render json: { message:"hello!", type: "basketballs", color: "orange", price: 15 }
+    render html: "
+     <title>HTML rendering!</title>
+     <h1>Sam's test HTML rendering</h1>
+     <p> testing testing testing testing testing testing testing testing
+      testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing
+     </p>
+     "
   end
 end
